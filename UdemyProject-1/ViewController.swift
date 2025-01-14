65fac443d071b8b6463da4c9da55ee6bffbc0be5
Agestia Deanara Tori <agestia.dtori@gmail.com>
2025-01-14 22:44:30 +0700

    Initial Commit

---
+++ UdemyProject-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UdemyProject-1
+//
+//  Created by Ages on 14/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
